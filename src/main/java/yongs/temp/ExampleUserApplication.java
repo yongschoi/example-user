ab9dc315591ffe5b3482c22aa5a690b0aae00c5a
yongschoi <yongschoi@naver.com>
2020-11-10 15:13:13 +0900

    minio host 설정

--- src/main/java/yongs/temp/ExampleUserApplication.java
+++ src/main/java/yongs/temp/ExampleUserApplication.java
@@ -3,14 +3,16 @@ package yongs.temp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ExampleUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleUserApplication.class, args);
 	}
-    @GetMapping("/")
+	@GetMapping("/")
     public String home() throws Exception {
         return "Hello, example-user";
     }
